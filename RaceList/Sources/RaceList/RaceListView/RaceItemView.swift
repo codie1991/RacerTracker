0cdc575e0adb36df7e20d33e9212dc07d54db4b8
Codie Westphall <codie@gettimely.com>
2023-09-30 18:05:24 +1300

    Improved UI for RaceList

--- RaceList/Sources/RaceList/RaceListView/RaceItemView.swift
+++ RaceList/Sources/RaceList/RaceListView/RaceItemView.swift
@@ -17,16 +17,6 @@ struct RaceItemView: View {
     
     var body: some View {
         HStack(alignment: .center) {
-            if raceItem.isRaceCompleted {
-                Image(systemName: "checkmark.circle")
-                    .imageScale(.large)
-                    .foregroundColor(.green)
-            } else {
-                Image(systemName: "flag.checkered")
-                    .imageScale(.large)
-                    .foregroundColor(.black)
-            }
-            
             VStack(alignment: .leading) {
                 Text("Round \(raceItem.round)")
                     .font(.caption)
@@ -52,16 +42,6 @@ struct HeroRaceItemView: View {
     
     var body: some View {
         HStack(alignment: .center) {
-            if raceItem.isRaceCompleted {
-                Image(systemName: "checkmark.circle")
-                    .imageScale(.large)
-                    .foregroundColor(.green)
-            } else {
-                Image(systemName: "flag.checkered")
-                    .imageScale(.large)
-                    .foregroundColor(.black)
-            }
-            
             VStack(alignment: .leading) {
                 Text("Round \(raceItem.round)")
                     .font(.caption)
@@ -69,34 +49,51 @@ struct HeroRaceItemView: View {
                 Text(raceItem.raceName)
                     .font(.title2)
                 Text(raceItem.circuit.circuitName)
-                    .font(.caption)
+                    .font(.body)
                 VStack(alignment: .center, content: {
-                    Text("Grand Prix Weekend")
+                    HStack {
+                        Image(systemName: "flag.checkered")
+                            .imageScale(.large)
+                            .foregroundColor(.white)
+                        Text("Grand Prix Weekend")
+                            .font(.subheadline)
+                            .fontWeight(.bold)
+                    }
+                    
                     Divider()
                         .overlay(.white)
                     HStack {
+                        Spacer()
                         VStack {
                             Text("\(raceItem.countdown?.day ?? 0)")
-                                .font(.headline)
+                                .font(.title)
+                                .fontWeight(.heavy)
                             Text("DAYS")
                                 .font(.subheadline)
                         }
+                        Spacer()
                         Divider()
                             .overlay(.white)
+                        Spacer()
                         VStack {
                             Text("\(raceItem.countdown?.hour ?? 0)")
-                                .font(.headline)
+                                .font(.title)
+                                .fontWeight(.heavy)
                             Text("HRS")
                                 .font(.subheadline)
                         }
+                        Spacer()
                         Divider()
                             .overlay(.white)
+                        Spacer()
                         VStack {
                             Text("\(raceItem.countdown?.minute ?? 0)")
-                                .font(.headline)
+                                .font(.title)
+                                .fontWeight(.heavy)
                             Text("MINS")
                                 .font(.subheadline)
                         }
+                        Spacer()
                     }
                 })
                 .padding(16)
